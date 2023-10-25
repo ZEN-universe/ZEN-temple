d71ed8ad013bba81f36dba5b8633a8b5e8433e7f
Vinzenz <muserv@ethz.ch>
2023-10-25 14:29:31 +0200

    Update solutions on startup

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.routers import cases, solutions
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import src.utils.updates
 
 app = FastAPI()
 
@@ -15,7 +16,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/public", StaticFiles(directory="public", follow_symlink=True), name="public")
+app.mount(
+    "/public", StaticFiles(directory="public", follow_symlink=True), name="public"
+)
 
 app.include_router(cases.router)
 app.include_router(solutions.router)
+
+try:
+    src.utils.updates.update_database()
+except Exception:
+    print("Could not update database, skipping.")
+
+src.utils.updates.create_components()
