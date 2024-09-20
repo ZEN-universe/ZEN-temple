dbffe70d6883128ba00a95025e417063de74b648
Vinzenz <muserv@ethz.ch>
2024-09-20 12:35:48 +0200

    Bumped version

--- explorer.py
+++ explorer.py
@@ -1,9 +1,16 @@
 import uvicorn
 import webbrowser
+from zen_temple.config import config
+import sys
 
-config = uvicorn.Config("src.main:app", port=8000, log_level="info")
-server = uvicorn.Server(config)
-webbrowser.open('http://localhost:8000/explorer', new=2)
 
-server.run()
+if __name__ == "__main__":
+    uvicorn_config = uvicorn.Config("zen_temple.main:app", port=8000, log_level="info")
+    server = uvicorn.Server(uvicorn_config)
+    webbrowser.open('http://localhost:8000/explorer', new=2)
+
+    if len(sys.argv) > 1:
+        config.SOLUTION_FOLDER = sys.argv[1]
+    
+    server.run()
 
