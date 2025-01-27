c969c5edec382f16f406a921299dbcb60bb1dddb
vinzenz-muser <muserv@ethz.ch>
2025-01-27 09:55:22 +0000

    Updated explorer

--- src/zen_temple/explorer/_app/immutable/chunks/stores.C_RjYdp6.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./entry.IS9A4qCW.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
